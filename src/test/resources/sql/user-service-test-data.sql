814daa5d89246d41c91b0eb951687e44405238a1
차정문 <ckwjdans4182@gmail.com>
2025-01-30 01:58:35 +0900

    test: userService 모든 메소드 테스트 코드 추가

--- src/test/resources/sql/user-service-test-data.sql
+++ src/test/resources/sql/user-service-test-data.sql
@@ -1,4 +1,4 @@
 insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
 values (1, 'ckwjdans4182@gmail.com', 'beegle', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
 insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
-values (2, 'ckwjdans1234@gmail.com', 'beegle2', 'Seoul', 'baaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'PENDING', 0);
+values (2, 'ckwjdans2@gmail.com', 'beegle2', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab', 'PENDING', 0);
